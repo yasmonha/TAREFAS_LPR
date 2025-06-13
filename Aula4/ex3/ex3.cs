590425a94aa51b2742f4d91bc35cf60125cad128
Yasmim Ribeiro <yasmim.ribeiro5005@gmail.com>
2025-06-13 09:34:22 -0300

    ex3.cs

--- Aula4/ex3/ex3.cs
+++ Aula4/ex3/ex3.cs
@@ -1,4 +1,15 @@
-﻿using System;
+/*Exercício 3) C#
+Suponha que você está desenvolvendo um jogo
+de RPG. Você é responsável por implementar a
+mecânica de habilidades especiais para
+diferentes classes de personagens. Cada classe
+tem suas próprias habilidades especiais.
+Crie um programa que permita ao jogador
+escolher uma classe de personagem e, em
+seguida, exiba suas habilidades especiais
+correspondentes. 
+*/
+using System;
 
 class Program {
 static void Main(){
